92d07cc95830b1e484a192f3c273ae64b552a817
Samuel Nzekwe <talk2samuelnzekwe@gmail.com>
2022-04-26 14:48:38 +0200

    layout, pages,install react-icons,set up hooks

--- src/App.js
+++ src/App.js
@@ -1,39 +1,134 @@
 import './index.css'
+import  './styles.scss'
+import  './style.js'
+import img from './assets/img_avatar.png'
+import {FaTimes, FaEdit} from 'react-icons/fa'
+import Contacts from './data/contacts'
+import { useState } from 'react'
+
+
+//import PropTypes from 'prop-types'
 
 const Footer = () => {
+  const footerYear = new Date().getFullYear()
   return (
-    <div>
-      <h1>This is my footer</h1>
+    <div className='footer'> 
+      <p>Copyright &copy; {footerYear} Samuel Nzekwe.</p>
     </div>
   )
 }
 
+//button component
 const Button = (props) => {
   return(
     <div>
-      <button>{props.label}</button>
+      <button className='bubbly-button'>{props.label}</button>
     </div>
   )
+
+  
 }
 
-
-
+//navbar component
 const Navbar = () => {
   return (
-    <div>
-      <h1>I am the Navbar Component</h1>
-      <Button label='Login' />
+    <div className="nav-container">
+
+      <div className="logo">
+        <h1>iKontact</h1>
+      </div>
+
+      <div className="links">
+        <ul>
+          <li>
+            <Button label='Home' />
+          </li>
+          <li>
+            <Button label='About' />
+          </li>
+          <li>
+            <Button label='My Contact List' />
+          </li>
+        </ul>
+      </div>
     </div>
   )
 }
 
 
+//contact card component
 
-function App() {
+const ContactCard = ({contact}) => {
+  
   return (
-    <div className="App">
+    <div className="chip">
+      <div className='card-text'>
+        <img src={img} alt="Person" width="96" height="96"/>
+        <p>Name: {contact.firstname}  {contact.lastname} </p>
+        <p id= 'chiptext'>Email: {contact.email}</p>
+      </div>
+
+      <div className='btn-container'>
+      <button className='btn'><FaEdit color='#ff0081'/>Edith</button>
+        <button className='btn'><FaTimes color='#ff0081'/>Delete</button>
+      </div>
+     
+    </div>
+  )
+}
+
+
+const ContactList = ({contacts}) => {
+  if(!contacts || contacts.length <= 0){
+    return <p>You do not have any contacts yet!</p>
+  } else {
+  return (
+    <div className="contact-list">
+      <h3>My Contacts</h3>
+        {contacts.map(contact => <ContactCard key={contact.id} contact={contact} /> )}     
+    </div>
+  )
+}}
+
+//form component
+
+const Form = ({contacts, onClick}) => {
+
+  return (
+    <div>
+      <h1>Add New Contact</h1>
+      <form>
+        <div>
+          firstname: <input />
+        </div>
+        <div>
+          lastname: <input />
+        </div>
+        <div>
+          email: <input />
+        </div>
+        <div>
+          <button type="submit" onClick={onClick}>add</button>
+        </div>
+      </form>
+    </div>
+  )
+}
+
+//main app component
+function App() {
+  const [contacts, setContact] = useState(Contacts)
+
+  const handleClick = () => {
+    console.log('clicked')
+  }
+  
+  return (
+    <div className="container">
       <Navbar />
-      <h1>Hello! This is from the App component of the contacts app project</h1>
+      
+      <ContactList  contacts={contacts}/>
+      <Form contacts={contacts} onClick={handleClick}/>
       <Footer />
     </div>
   );
---
+++ src/data/contacts.js
@@ -0,0 +1,46 @@
+const Contacts =  [
+
+    {
+      id: 1,
+      firstname: 'Mike',
+      email: 'mike@gmail.com',
+      lastname: 'Rose'
+    },
+
+    {
+      id: 2,
+      firstname: 'James',
+      email: 'james@gmail.com',
+      lastname: 'Rogers'
+    },
+    
+    {
+      id: 3,
+      firstname: 'Samuel',
+      email: 'samuel@gmail.com',
+      lastname: 'Nzekwe'
+    },
+
+    {
+      id: 4,
+      firstname: 'Simon',
+      email: 'simon@gmail.com',
+      lastname: 'Ibo'
+    },
+
+    {
+      id: 5,
+      firstname: 'efnoweoi',
+      email: 'simon@gmail.com',
+      lastname: 'Ibo'
+    },
+
+    {
+      id: 6,
+      firstname: 'Simon',
+      email: 'simon@gmail.com',
+      lastname: 'Ibo'
+    },
+  ]
+
+export default Contacts  
